8bd69cc1cd794021e4a01f7f66ebe9f8ddb49ffc
na0495 <saadmrabet008@gmail.com>
2022-10-22 10:37:01 +0100

    fix lint & fix issue with 3dAvatard

--- src/assets/ThreedAvatar.tsx
+++ src/assets/ThreedAvatar.tsx
@@ -1,87 +1,37 @@
-import { useRef, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
+import modelSrc from "../assets/model.glb?url";
 
-// --------------------------------------------------
-
-export function ThreedAvatar(props: any) {
+export function ThreedAvatar(props) {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF(
-    "/src/assets/animated-saad.glb"
-  );
+  const { nodes, materials, animations } = useGLTF(modelSrc);
+  const { actions } = useAnimations(animations, group);
 
-  // const { actions } = useAnimations(animations, group);
-
-  // useEffect(() => {
-  //   actions["Armature|mixamo.com|Layer0"].play();
-  // });
+  useEffect(() => {
+    actions["Armature|mixamo.com|Layer0"].play();
+  });
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <primitive object={nodes.Hips} />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Body.geometry}
-        material={materials.Wolf3D_Body}
-        skeleton={nodes.Wolf3D_Body.skeleton}
-      />
-      <skinnedMesh
-        name="EyeLeft"
-        geometry={nodes.EyeLeft.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeLeft.skeleton}
-        morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="EyeRight"
-        geometry={nodes.EyeRight.geometry}
-        material={materials.Wolf3D_Eye}
-        skeleton={nodes.EyeRight.skeleton}
-        morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-        morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-      />
-      <skinnedMesh
-        name="Wolf3D_Teeth"
-        geometry={nodes.Wolf3D_Teeth.geometry}
-        material={materials.Wolf3D_Teeth}
-        skeleton={nodes.Wolf3D_Teeth.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-        material={materials.Wolf3D_Outfit_Bottom}
-        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Glasses.geometry}
-        material={materials.Wolf3D_Glasses}
-        skeleton={nodes.Wolf3D_Glasses.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-        material={materials.Wolf3D_Outfit_Footwear}
-        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Outfit_Top.geometry}
-        material={materials.Wolf3D_Outfit_Top}
-        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-      />
-      <skinnedMesh
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
-        name="Wolf3D_Head"
-        geometry={nodes.Wolf3D_Head.geometry}
-        material={materials.Wolf3D_Skin}
-        skeleton={nodes.Wolf3D_Head.skeleton}
-        morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-        morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-      />
+      <group name="Scene">
+        <group name="Armature">
+          <primitive object={nodes.Hips} />
+          <skinnedMesh
+            name="Wolf3D_Avatar_Transparent"
+            geometry={nodes.Wolf3D_Avatar_Transparent.geometry}
+            material={materials["Wolf3D_Avatar_Transparent.001"]}
+            skeleton={nodes.Wolf3D_Avatar_Transparent.skeleton}
+          />
+          <skinnedMesh
+            name="Wolf3D_Avatar"
+            geometry={nodes.Wolf3D_Avatar.geometry}
+            material={materials["Wolf3D_Avatar.002"]}
+            skeleton={nodes.Wolf3D_Avatar.skeleton}
+            morphTargetDictionary={nodes.Wolf3D_Avatar.morphTargetDictionary}
+            morphTargetInfluences={nodes.Wolf3D_Avatar.morphTargetInfluences}
+          />
+        </group>
+      </group>
     </group>
   );
 }
-
-useGLTF.preload("/src/assets/animated-saad.glb");
