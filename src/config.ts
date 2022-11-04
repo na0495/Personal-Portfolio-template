2801f155ad67912a36dc39795dab5353004bb3a6
na0495 <saadmrabet008@gmail.com>
2022-11-04 17:05:48 +0000

    quick fix

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const GITHUB_USERNAME = "na0495";
 
-export const googleAnlyticId = process.env.REACT_APP_GA_ID || "";
+export const googleAnlyticId = import.meta.env.VITE_REACT_APP_GA_ID || "";
