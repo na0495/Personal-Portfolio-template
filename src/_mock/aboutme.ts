2a5bb1326e221fe6f55ec1eb6b466e15698f338b
na0495 <saadmrabet008@gmail.com>
2022-11-04 20:21:27 +0000

    📦 update packages, and update about me 👔

--- src/_mock/aboutme.ts
+++ src/_mock/aboutme.ts
@@ -10,32 +10,42 @@ export const aboutMe = {
   details: [
     {
       id: 1,
-      text: `I'm Mrabet saâd, a ${getAge()} years old programmer, from Tangier Morocco 🇲🇦.`,
+      text: ` I'm Mrabet saâd, a ${getAge()} years old programmer, from Tangier Morocco 🇲🇦. `,
       icon: "🧔🏻",
     },
     {
       id: 2,
-      text: "I got my bachelor's degree in Computer Engineering at 2021.",
+      text: " I got my bachelor's degree in Computer Engineering at 2021. ",
       icon: "🧑🏼‍🎓",
     },
     {
       id: 3,
-      text: " I enjoy being challenged and engaging with projects that require me to work outside my comfort zone and knowledge set, as continuing to learn new languages and development techniques.",
+      text: `
+              I enjoy being challenged and engaging with projects that require me to work outside
+              my comfort zone and knowledge set, as continuing to learn new languages and 
+              development techniques.
+           `,
       icon: "🎯",
     },
     {
       id: 4,
-      text: "I'm fluent in many programming languages, especially in JavaScript/TypeScript & Python.",
+      text: " I'm fluent in many programming languages, especially in Js/Ts & Python. ",
       icon: "💻",
     },
     {
       id: 5,
-      text: "My field of Interest are building new  Web Technologies and Products and also in areas related Mobile application and Natural Language Processing.",
+      text: `
+              My field of Interest are building new  Web Technologies and Products and also in 
+              areas related Mobile application and Natural Language Processing.
+           `,
       icon: "🔎",
     },
     {
       id: 6,
-      text: "whenever possible, i also apply my passion for developing products with multiple different frameworks and technologies, like Django, React, Angular, Vite, Redux, Node.js ...",
+      text: `
+              whenever possible, i also apply my passion for developing products with multiple 
+              different frameworks and technologies, like Django, React, Angular, Vite, Redux, Node.js ...
+           `,
       icon: "💭",
     },
   ],
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,61 @@
 // Mantine
-import { Container, Title, Text } from "@mantine/core";
+import { Container, Text, Title } from "@mantine/core";
+import { Prism } from "@mantine/prism";
 // Components
 import BoxWrapper from "./BoxWrapper";
 // d_mock
+import { useMediaQuery } from "@mantine/hooks";
 import { aboutMe } from "../_mock/aboutme";
 
 // ----------------------------------------------------------------------------
 
+const aboutMeCode = `import Saad from "saad/core/"
+
+// -----------------------------------------------------------------------------
+
+export default function AboutMe() {
+  return (
+    <Saad ${aboutMe.details.map(
+      (item: any) =>
+        `
+      ${item.icon} = {${item.text}}`
+    )}
+    />
+  );
+}`;
+
 export default function About() {
+  const match = useMediaQuery("(max-width: 1000px)");
   return (
     <Container px="xl" size="lg">
       <BoxWrapper withBackground={false}>
-        <Title
-          order={3}
-          sx={(theme) => ({
-            marginBottom: 25,
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.yellow[5]
-                : theme.colors.orange[5],
-          })}
-        >
-          {aboutMe.title}
-        </Title>
+        {!match ? (
+          <Prism language="tsx">{aboutMeCode}</Prism>
+        ) : (
+          <>
+            <Title
+              order={3}
+              sx={(theme) => ({
+                marginBottom: 25,
+                color:
+                  theme.colorScheme === "dark"
+                    ? theme.colors.yellow[5]
+                    : theme.colors.orange[5],
+              })}
+            >
+              {aboutMe.title}
+            </Title>
 
-        {aboutMe.details.map((item: any, index: number) => (
-          <div key={index}>
-            <Text size="lg" key={item.id} className="text" mt={5}>
-              <span style={{ marginRight: 5 }}>{item.icon}</span>
-              {item.text}
-            </Text>
-          </div>
-        ))}
+            {aboutMe.details.map((item: any, index: number) => (
+              <div key={index}>
+                <Text size="lg" key={item.id} className="text" mt={5}>
+                  <span style={{ marginRight: 5 }}>{item.icon}</span>
+                  {item.text}
+                </Text>
+              </div>
+            ))}
+          </>
+        )}
       </BoxWrapper>
     </Container>
   );
