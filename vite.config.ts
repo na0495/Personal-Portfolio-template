d302dcda011457b97a8defcfab8646001eadc4d2
na0495 <saadmrabet008@gmail.com>
2022-11-30 23:39:15 +0000

    🔧 update config for ga & add metas into the index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgLoader from "vite-svg-loader";
 import svgr from "@honkhonk/vite-plugin-svgr";
+import VitePluginHtmlEnv from 'vite-plugin-html-env'
+
+// ------------------------------------
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader(), svgr()],
+  plugins: [react(), svgLoader(), svgr(), VitePluginHtmlEnv(),      VitePluginHtmlEnv({
+    compiler: true
+  })],
   base: "./",
+
 });
