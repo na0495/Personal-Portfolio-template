0ea1bb366cc54e520643834d227668835525851c
Egon001 <saadmrabet008@gmail.com>
2022-12-12 17:57:45 +0000

    update vite lib version to 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import svgLoader from "vite-svg-loader";
 import svgr from "@honkhonk/vite-plugin-svgr";
 // ------------------------------------
